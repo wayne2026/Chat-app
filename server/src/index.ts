d574b6cd7cdc6b2d1f06a8e7745b2ff95e3b01af
Nilkamal Sarma <nilkamalsarma12@gmail.com>
2026-01-12 13:37:23 +0530

    CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000','https://chat-client-xwdq.onrender.com/'],
     credentials: true
 }));
 app.use(express.json());
